3d7e431d031ef3696ef8692acff97eca438d8b66
ryan <602276788@qq.com>
2018-11-12 23:53:11 +0800

    spring boot工程搭建

---
+++ src/main/java/com/template/ryan/RyanApplication.java
@@ -0,0 +1,12 @@
+package com.template.ryan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RyanApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RyanApplication.class, args);
+	}
+}
---
+++ src/test/java/com/template/ryan/RyanApplicationTests.java
@@ -0,0 +1,16 @@
+package com.template.ryan;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class RyanApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
