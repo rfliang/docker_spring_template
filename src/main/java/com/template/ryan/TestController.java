fddfd01bdf3996b09653c73f868ca202a358e787
rfliang <602276788@qq.com>
2018-11-15 15:51:48 +0800

    更改类型

--- src/main/java/com/template/ryan/TestController.java
+++ src/main/java/com/template/ryan/TestController.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class TestController {
 
-    @RequestMapping(value = "/aa", method = RequestMethod.GET)
+    @RequestMapping(value = "/aa", method = RequestMethod.POST)
     public String hello() {
         return "Hello! Docker!";
     }
